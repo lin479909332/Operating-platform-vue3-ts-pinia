d340f6901231fef3e00cf4cbe7585656a0e70c66
lin479909332 <lin479909332@gmail.com>
2023-06-11 15:27:26 +0800

    feat(路由鉴权): 芯智能登录与未登录情况下的路由鉴权配置
    1、删除旧版在home组件获取用户信息
    2、修改nprogress的命名
    3、关闭nprogress的加载球

--- src/permission.ts
+++ src/permission.ts
@@ -3,15 +3,54 @@
 
 import router from '@/router'
 import { RouteLocationNormalized } from 'vue-router'
-import nProgress from 'nprogress'
+import setting from './setting'
+import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
-
+// 关闭右侧的加载球
+nprogress.configure({ showSpinner: false })
+import useUserStore from '@/store/modules/user'
+import pinia from './store'
+// 这里拿不到要导入pinia
+let userStore = useUserStore(pinia)
 // 全局前置守卫
 router.beforeEach(
-  (to: RouteLocationNormalized, from: RouteLocationNormalized, next) => {
-    console.log(to)
+  async (to: RouteLocationNormalized, from: RouteLocationNormalized, next) => {
+    document.title = `${setting.title} - ${to.meta.title}`
     console.log(from)
-    nProgress.start()
+    nprogress.start()
+    // 获取用户token判断用户是否登录
+    let token = userStore.token
+    let username = userStore.username
+    if (token) {
+      // 用户已登录
+      // 不让跳转到login，重定向到home
+      if (to.path === '/login') {
+        next({ path: '/' })
+      } else {
+        // 其余放行(存在用户名的情况下)
+        if (username) {
+          next()
+        } else {
+          try {
+            // 重新获取用户信息
+            await userStore.userInfo()
+          } catch (error) {
+            // token过期:获取不到用户信息了或者用户手动修改本地存储token
+            // 退出登录并回到首页,并保留要去的path
+            userStore.userLogout()
+            next({ path: '/login', query: { redirect: to.path } })
+          }
+        }
+      }
+    } else {
+      // 用户未登录
+      if (to.path === '/login') {
+        next()
+      } else {
+        // 跳转到login同时保存想要跳转的path到query里
+        next({ path: '/login', query: { redirect: to.path } })
+      }
+    }
     next()
   },
 )
@@ -21,6 +60,6 @@ router.afterEach(
   (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
     console.log(to)
     console.log(from)
-    nProgress.done()
+    nprogress.done()
   },
 )
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,13 +3,6 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import useUserStore from '@/store/modules/user'
-let userStore = useUserStore()
-// 组件挂载完毕后获取用户信息
-onMounted(() => {
-  userStore.userInfo()
-})
 </script>
 
 <style lang="less" scoped></style>
