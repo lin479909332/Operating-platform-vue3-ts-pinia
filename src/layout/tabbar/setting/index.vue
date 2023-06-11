1a95a6284ba1bf2b74b82d4f6500a2e7689d59a5
lin479909332 <lin479909332@gmail.com>
2023-06-11 12:57:46 +0800

    feat(导航栏全屏按钮): 新增浏览器全屏模式

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -5,7 +5,12 @@
     icon="Refresh"
     @click="updateRefresh"
   ></el-button>
-  <el-button circle size="small" icon="FullScreen"></el-button>
+  <el-button
+    circle
+    size="small"
+    icon="FullScreen"
+    @click="fullScreen"
+  ></el-button>
   <el-button circle size="small" icon="Setting"></el-button>
   <img
     src="../../../../public/logo.jpg"
@@ -33,6 +38,18 @@ let layoutSettingStore = useLayoutSettingStore()
 // 用户点击刷新按钮后修改仓库里的值
 const updateRefresh = () => {
   layoutSettingStore.refresh = !layoutSettingStore.refresh
+}
+// 用户点击全屏按钮
+const fullScreen = () => {
+  // DOM对象的一个属性:可以用来判断当前是不是全屏模式[全屏:true,不是全屏:false]
+  let full = document.fullscreenElement
+  if (!full) {
+    // 切换为全屏模式
+    document.documentElement.requestFullscreen()
+  } else {
+    // 退出全屏模式
+    document.exitFullscreen()
+  }
 }
 </script>
 <script lang="ts">
