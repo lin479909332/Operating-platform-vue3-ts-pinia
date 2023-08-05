cc52ee14858e7e329f9cde643e22bb9cae762cf8
lin479909332 <lin479909332@gmail.com>
2023-08-05 23:43:56 +0800

    feat(数据大屏): 完成了数据大屏的顶部搭建
    1、完成了数据大屏的顶部搭建
    2、新增插件moment用于显示时间
    3、完善了数据大屏静态框架
    4、新增数据大屏静态图片资源

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,72 @@
 <template>
-  <div>数据大屏</div>
+  <div class="container">
+    <!-- 数据大屏展示区域 -->
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">左侧</div>
+        <div class="center">中间</div>
+        <div class="right">右侧</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {} from 'vue'
+import { ref, onMounted } from 'vue'
+import Top from './components/top/index.vue'
+// 获取数据大屏盒子的dom元素
+let screen = ref()
+
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+
+// 定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+
+// 监听视口发生变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url('./images/bg.png') no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    // 设置基点
+    transform-origin: left top;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
