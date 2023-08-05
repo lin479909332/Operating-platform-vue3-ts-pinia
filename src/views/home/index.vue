f031bf640beea55036c07916be0e245f0ed63460
lin479909332 <lin479909332@gmail.com>
2023-08-05 21:00:02 +0800

    feat(首页): 新增了首页的展示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>一级路由home</div>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar" />
+      <div class="bottom">
+        <h3 class="title">{{ userStore.username }},{{ getTime() }}好</h3>
+        <p class="sub-title">欢迎您来到甄选运营平台</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="welcome">
+    <svg-icon name="welcome" width="600px" height="500px"></svg-icon>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// 引入用户相关的仓库
+import useUserStore from '@/store/modules/user'
+// 获取存储用户信息的仓库对象
+let userStore = useUserStore()
+import { getTime } from '@/utils/time'
+</script>
 
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+.box {
+  display: flex;
+  .avatar {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+  }
+  .bottom {
+    margin: 20px 0 0 30px;
+    .title {
+      font-size: 30px;
+      font-weight: 900;
+      margin-bottom: 30px;
+    }
+    .sub-title {
+      font-style: italic;
+      font-weight: 300;
+      color: #999;
+    }
+  }
+}
+.welcome {
+  display: flex;
+  justify-content: center;
+}
+</style>
