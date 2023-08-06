85ca476eab655a628f755b61d283164fcb1e523a
lin479909332 <lin479909332@gmail.com>
2023-08-06 16:07:38 +0800

    style(地图相关): 格式化代码

--- src/views/screen/components/line/index.vue
+++ src/views/screen/components/line/index.vue
@@ -1,12 +1,9 @@
 <template>
-<div>折线图</div>
+  <div>折线图</div>
 </template>
 
-<script lang='ts' setup>
-import { } from 'vue'
-
+<script lang="ts" setup>
+import {} from 'vue'
 </script>
- 
-<style lang="scss" scoped>
-  
-</style>
+
+<style lang="scss" scoped></style>
