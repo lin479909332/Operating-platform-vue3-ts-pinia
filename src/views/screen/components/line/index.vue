0878c59b3ecced41b90d897176f610574ff9f1c2
lin479909332 <lin479909332@gmail.com>
2023-08-06 16:20:05 +0800

    feat(数据大屏): 完成了游客趋势折线图的渲染

--- src/views/screen/components/line/index.vue
+++ src/views/screen/components/line/index.vue
@@ -1,9 +1,113 @@
 <template>
-  <div>折线图</div>
+  <div class="box5">
+    <div class="title">
+      <p>未来7天游客趋势图</p>
+      <img src="../../images/dataScreen-title.png" alt="" />
+    </div>
+    <div class="charts" ref="line"></div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {} from 'vue'
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts'
+// 获取节点
+let line = ref()
+onMounted(() => {
+  let myCharts = echarts.init(line.value)
+  myCharts.setOption({
+    //标题组件
+    title: {
+      text: '访问量',
+    },
+    //x|y轴
+    xAxis: {
+      type: 'category',
+      //两侧不留白
+      boundaryGap: false,
+      //分割线不要
+      splitLine: {
+        show: false,
+      },
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      //轴线的设置
+      axisLine: {
+        show: true,
+      },
+      //刻度
+      axisTick: {
+        show: true,
+      },
+    },
+    yAxis: {
+      splitLine: {
+        show: false,
+      },
+      //轴线的设置
+      axisLine: {
+        show: true,
+      },
+      //刻度
+      axisTick: {
+        show: true,
+      },
+    },
+    grid: {
+      left: 40,
+      top: 0,
+      right: 20,
+      bottom: 20,
+    },
+    //系列
+    series: [
+      {
+        type: 'line',
+        data: [320, 2240, 166, 2299, 321, 1990, 1100],
+        //平滑曲线的设置
+        smooth: true,
+        //区域填充样式
+        areaStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: 'gold', // 0% 处的颜色
+              },
+              {
+                offset: 1,
+                color: '#0f1751', // 100% 处的颜色
+              },
+            ],
+            global: false, // 缺省为 false
+          },
+        },
+      },
+    ],
+  })
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box5 {
+  width: 100%;
+  height: 100%;
+  background: url('../../images/dataScreen-main-cb.png') no-repeat;
+  background-size: 100% 100%;
+  margin: 0 20px;
+  .title {
+    margin-left: 20px;
+    p {
+      color: #fff;
+      font-size: 20px;
+    }
+  }
+  .charts {
+    height: calc(100% - 40px);
+  }
+}
+</style>
